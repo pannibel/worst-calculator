b4ab483cb3de574594c0d0e2cf83e34bfad920e0
Panni Belevski <pannibel@gmail.com>
2022-08-28 17:19:02 +0200

    trying to make rounding work

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ let operator;
 let checked;
 let rounding;
 let result;
-let resultText;
 
 document.querySelector("#calculate").addEventListener("click", readNumbers);
 document.querySelector("#calculate").addEventListener("click", opFunc);
 document.querySelector("#calculate").addEventListener("click", opCalc);
-document.querySelector("#doround").addEventListener("click", roundTo);
+document.querySelector("#doround").addEventListener("click", roundOrNot);
 
 // READ A NUMBER FROM AN IMPUT FIELD
 
@@ -51,42 +50,93 @@ function opCalc() {
         result = firstnumber / secondnumber;
     };
 
-    console.log(result)
+    console.log(result);
+    writeResult();
 }
 
 
 // CHECKING IF A CHECKBOX IS SELECTED
 
-function roundTo() {
+function roundOrNot() {
 
     checked = document.querySelector("#doround").checked;
     console.log("rounding or no rounding");
 
     if (checked) {
         rounding = Number(document.querySelector("#decimals").value);
-        writeResult();
         console.log("yes rounding");
         console.log(rounding);
-    };
+        doRound();
+        }
     if (!checked) {
-        console.log("no rounding")
-    };
-}
+        console.log("no rounding");
+}}
 
 // ROUNDING TO A NUMBER OF DECIMALS
 
-
-// WRITING A RESULT INTO AN IMPUT FIELD
-
-function writeResult() {
-
+function doRound() {
+    console.log("round to", rounding);
+    if (rounding === 0) {
+        result = result.toFixed();
+        result = Number(result);
+        console.log("rounded result is:", typeof result, result)
+    };
+    if (rounding === 1) {
+        let converted1 = result.toFixed(1);
+        result = Number(converted1);
+        console.log("rounded result is:", typeof result, result)
+    };
+    if (rounding === 2) {
+        let converted2 = result.toFixed(2);
+        result = Number(converted2);
+        console.log("rounded result is:", typeof result, result)
+    };
+    if (rounding === 3) {
+        let converted3 = result.toFixed(3);
+        result = Number(converted3);
+        console.log("rounded result is:", typeof result, result)
+    };
+    if (rounding === 4) {
+        let converted4 = result.toFixed(1);
+        result = Number(converted4);
+        console.log("rounded result is:", typeof result, result)
+    };
+    if (rounding === 5) {
+        let converted5 = result.toFixed(5);
+        result = Number(converted5);
+        console.log("rounded result is:", typeof result, result)
+    };
+    if (rounding === 6) {
+        let converted6 = result.toFixed(6);
+        result = Number(converted6);
+        console.log("rounded result is:", typeof result, result)
+    };
+    if (rounding === 7) {
+        let converted7 = result.toFixed(7);
+        result = Number(converted7);
+        console.log("rounded result is:", typeof result, result)
+    };
+    if (rounding === 8) {
+        let converted8 = result.toFixed(8);
+        result = Number(converted8);
+        console.log("rounded result is:", typeof result, result)
+    };
 }
 
+// WRITING A RESULT INTO AN IMPUT FIELD AND APPENDING TO AN HTML LIST
 
-// APPENDING TO AN HTML LIST
+function writeResult() {
+    document.querySelector("#firstnumber").value = result;
+    document.querySelector("ul").append(result);
+
+    listScroll();
+}
 
 
 // SCROLLING A LIST TO THE BOTTOM
 
+function listScroll() {
+    document.querySelector("#results").scrollTo(0, 100000);
+}
 
 // CLEARING A LIST
